c4e3e44e730b157e8f9fde494026d4ad05c1a840
celemonnier <cesario.le-monnier@etu.u-bordeaux.fr>
2021-03-28 22:36:22 +0200

    histo

--- backend/src/main/java/pdl/backend/Algo.java
+++ backend/src/main/java/pdl/backend/Algo.java
@@ -4,6 +4,9 @@ import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.array.ArrayImgs;
+
+import java.util.List;
+
 import io.scif.SCIFIO;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
@@ -24,9 +27,7 @@ public class Algo {
         final Cursor<UnsignedByteType> cR = inputR.cursor();
         final Cursor<UnsignedByteType> cG = inputG.cursor();
         final Cursor<UnsignedByteType> cB = inputB.cursor();
-
-
-		
+	
 		while (cR.hasNext() && cG.hasNext() && cB.hasNext()) {
 			cR.fwd();
             cG.fwd();
@@ -61,4 +62,109 @@ public class Algo {
 		}
 
 	}
+
+	public static void EgalisationHistogramme(Img<UnsignedByteType> img) {
+		final IntervalView<UnsignedByteType> inputR = Views.hyperSlice(img, 2, 0);
+        final IntervalView<UnsignedByteType> inputG = Views.hyperSlice(img, 2, 1);
+        final IntervalView<UnsignedByteType> inputB = Views.hyperSlice(img, 2, 2);
+        final Cursor<UnsignedByteType> cR = inputR.cursor();
+        final Cursor<UnsignedByteType> cG = inputG.cursor();
+        final Cursor<UnsignedByteType> cB = inputB.cursor();
+		final Cursor<UnsignedByteType> cRcopy = cR.copyCursor();
+        final Cursor<UnsignedByteType> cGcopy = cG.copyCursor();
+        final Cursor<UnsignedByteType> cBcopy = cB.copyCursor();
+
+		int[] histogramme=new int[256];
+		int[] histogrammeCumule=new int[256];
+		float[] hsv={0,0,0};
+		while (cR.hasNext() && cG.hasNext() && cB.hasNext()) {
+			cR.fwd();
+            cG.fwd();
+            cB.fwd();
+			final UnsignedByteType valR = cR.get();
+			final UnsignedByteType valG = cG.get();
+			final UnsignedByteType valB = cB.get();
+			rgbToHsv(valR.get(),valG.get(),ValB.get(),hsv);
+			h.add(hsv[0]);
+			v.add(hsv[2]);
+			histogramme[hsv[1]]++;
+		}
+		for(int i=0; i<256; i++){
+			if(i==0){
+				histogrammeCumule[0]=histogramme[0];
+			}else{
+				histogrammeCumule[i]=histogrammeCumule[i-1]+histogramme[i];
+			}
+		}
+		int[] rgb={0,0,0};
+		while(cRcopy.hasNext() && cGcopy.hasNext() && cBcopy.hasNext()){
+			cRcopy.fwd();
+            cGcopy.fwd();
+            cBcopy.fwd();
+			final UnsignedByteType valR = cR.get();
+			final UnsignedByteType valG = cG.get();
+			final UnsignedByteType valB = cB.get();
+			rgbToHsv(valR.get(),valG.get(),ValB.get(),hsv);
+			int s=(histogrammeCumule[hsv[1]]*255/histogrammeCumule[255]);
+			
+			hsvToRgb(hvs[0],s,hsv[2],rgb);
+			valR.set(rgb[0]);
+			valG.set(rgb[1]);
+			valB.set(rgb[2]);
+		}
+	}
+
+	
+    void rgbToHsv(int r, int g, int b, float[] hsv){
+		float h,s,v;
+		int max=max(max(r,g),b);
+		int min=min(min(r,g),b);
+		int Maxmin=max-min;
+		if(max==min){
+			h=0;
+		}else if(max==r){
+			h=(60*(g-b)/Maxmin) % 360;
+		}else if(max==g){
+			h=60*(b-r)/Maxmin +120;
+		}else if(max==b){
+			h=60*(r-g)/Maxmin + 240;
+		}
+		if(max==0){
+			s=0;
+		}else{
+			s=1-(min/max);
+		}
+		v=max;
+		hsv[0]=h;
+		hsv[1]=s;
+		hsv[2]=v;
+	}
+
+    void hsvToRgb(float h, float s, float v, int[] rgb){
+		float hi=((int)(h/60)) %6;
+		float f=h/60 -hi;
+		float l = v * (1-s);
+		float m = v * (1-f*s);
+		float n = v * (1 -(1-f)*s);
+		if(hi=0){
+			int[] myArray={v,n,l};
+		}
+		if(hi=1){
+			int[] myArray={m,v,l};
+		}
+		if(hi=2){
+			int[] myArray={l,v,n};
+		}
+		if(hi=3){
+			int[] myArray={l,m,v};
+		}
+		if(hi=4){
+			int[] myArray={n,l,v};
+		}
+		if(hi=5){
+			int[] myArray={v,l,m};
+		}
+		rgb=myArray;
+	}
+
 }
